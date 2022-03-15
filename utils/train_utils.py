f6ccdeb0e84144e95769b15c0316b76d6c800083
Yang Xiao <swagshaw1998@gmail.com>
2022-03-15 20:59:19 +0800

    :taba: initialize the utils

---
+++ utils/train_utils.py
@@ -0,0 +1,61 @@
+"""
+# !/usr/bin/env python
+-*- coding: utf-8 -*-
+@Time    : 2022/1/25 下午10:50
+@Author  : Yang "Jan" Xiao 
+@Description : train_utils
+"""
+import torch_optimizer
+from torch import optim
+from networks.tcresnet import MFCC_TCResnet
+
+
+def select_optimizer(opt_name, lr, model, sched_name="cos"):
+    if opt_name == "adam":
+        opt = optim.Adam(model.parameters(), lr=lr, weight_decay=1e-6)
+    elif opt_name == "radam":
+        opt = torch_optimizer.RAdam(model.parameters(), lr=lr, weight_decay=0.00001)
+    elif opt_name == "sgd":
+        opt = optim.SGD(
+            model.parameters(), lr=lr, momentum=0.9, nesterov=True, weight_decay=1e-4
+        )
+    else:
+        raise NotImplementedError("Please select the opt_name [adam, sgd]")
+
+    if sched_name == "cos":
+        scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(
+            opt, T_0=1, T_mult=2, eta_min=lr * 0.01
+        )
+    elif sched_name == "anneal":
+        scheduler = optim.lr_scheduler.ExponentialLR(opt, 1 / 1.1, last_epoch=-1)
+    elif sched_name == "multistep":
+        scheduler = optim.lr_scheduler.MultiStepLR(
+            opt, milestones=[30, 60, 80, 90], gamma=0.1
+        )
+    else:
+        raise NotImplementedError(
+            "Please select the sched_name [cos, anneal, multistep]"
+        )
+
+    return opt, scheduler
+
+
+def select_model(model_name, total_class_num=None):
+    # load the model.
+    config = {
+        "tcresnet8": [16, 24, 32, 48],
+        "tcresnet14": [16, 24, 24, 32, 32, 48, 48]
+    }
+
+    if model_name == "tcresnet8":
+        model = MFCC_TCResnet(bins=40, channels=config[model_name], channel_scale=1,
+                              num_classes=total_class_num)
+    elif model_name == "tcresnet14":
+        model = MFCC_TCResnet(bins=40, channels=config[model_name], channel_scale=1,
+                              num_classes=total_class_num)
+    else:
+        model = None
+
+    return model
+
+
---
+++ utils/utils.py
@@ -0,0 +1,55 @@
+"""
+# !/usr/bin/env python
+-*- coding: utf-8 -*-
+@Time    : 2022/3/15 下午5:43
+@Author  : Yang "Jan" Xiao 
+@Description : utils
+"""
+import torch
+
+
+class ColorEnum:
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+
+
+def print_color(color, *msgs):
+    print(color, *msgs, ColorEnum.END)
+
+
+def readlines(datapath):
+    # Read all the lines in a text file and return as a list
+    with open(datapath, 'r') as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def parameter_number(model):
+    num_params = 0
+    for tensor in list(model.parameters()):
+        tensor = tensor.view(-1)
+        num_params += len(tensor)
+    print(">>>  ", num_params)
+
+
+def prepare_device(n_gpu_use):
+    """
+    setup GPU device if available, move model into configured device
+    """
+    n_gpu = torch.cuda.device_count()
+    if n_gpu_use > 0 and n_gpu == 0:
+        print_color(ColorEnum.YELLOW, "There\'s no GPU available on this machine, training will be performed on CPU.")
+        n_gpu_use = 0
+    if n_gpu_use > n_gpu:
+        print_color(ColorEnum.YELLOW,
+                    "Warning: The number of GPU\'s configured to use is {}, but only {} are available on this machine.".format(
+                        n_gpu_use, n_gpu))
+        n_gpu_use = n_gpu
+    device = torch.device("cuda" if n_gpu_use > 0 else "cpu")
+    list_ids = list(range(n_gpu_use))
+    return device, list_ids
